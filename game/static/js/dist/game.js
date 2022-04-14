2025fbbe5af73a0737c49e1d24cf7ca2848affb4
ye wang <2590179717@qq.com>
2022-04-14 22:57:27 +0800

    Get Images and Audios

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -1,7 +1,15 @@
-
 export class MainGame {
-    constructor(id) {
-        this.id = id;
-        console.log("Succeed!!");
+            constructor(id) {
+                        this.id = id;
+                        console.log('Succ');
+                        this.$test = $(`
+                        <h1>hahaha</h1>
+                        <img src="../static/material/words/difficult/7/7.mp3"></img>
+                        <audio controls src="../static/material/words/difficult/7/7.mp3"></audio>
+                                `);
+                this.$test.show();
+                this.star();
+             }
+    star() {
     }
 }
---
+++ game/static/material/words/index.py
@@ -0,0 +1,74 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import os
+
+
+def GetWords(path):
+    words = []
+    with open(path, 'r', encoding='utf-8') as pf:
+        for line in pf:
+            if line == '\n':
+                continue
+            words.append(line)
+    for i in range(len(words)):
+        words[i] = words[i].replace(' ', '')
+        words[i] = words[i].replace('\n', '')
+    return words
+
+
+def GetUrl(word):
+    images = []
+    audio = []
+    index = 'https://hanyu.baidu.com/zici/s?wd=' + word
+    r = requests.get(index)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    # audio
+    aList = soup.select('#pinyin > span > a')
+    for a in aList:
+        audio.append(a.get('url'))
+    # images
+    url = soup.select('#header-img > div')
+    url = url[0].get('style')
+    url = url.replace('background-image: url(', '')
+    url = url.replace(')', '')
+    for _ in range(len(audio)):
+        images.append(url)
+    return images, audio
+
+
+def GetContent(url):
+    r = requests.get(url)
+    return r.content
+
+
+def ParseAndSave(images, audio, ct, path):
+    for item in zip(images, audio):
+        with open('{}/{}.png'.format(path, ct), 'wb') as pim:
+            pim.write(GetContent(item[0]))
+        with open('{}/{}.mp3'.format(path, ct), 'wb') as pa:
+            pa.write(GetContent(item[1]))
+        ct += 1
+    return ct
+
+
+def MainFile(fname):
+    words = GetWords(fname)
+    path = 'difficult'
+    if fname == 'wordsEasy.txt':
+        path = 'common'
+    k = 1
+    ct = 0
+    for wordSame in words:
+        newPath = './{}/{}'.format(path, k)
+        k += 1
+        os.mkdir(newPath)
+        for word in wordSame:
+            images, audio = GetUrl(word)
+            ct = ParseAndSave(images, audio, ct, newPath)
+        ct = 0
+
+
+if __name__ == '__main__':
+    MainFile('wordsEasy.txt')
+    MainFile('wordsHard.txt')
